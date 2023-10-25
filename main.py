16a2ad664f835086075e92b9d17a98c3f47f5b2a
Sofjaklopcova <sofjk@seznam.cz>
2023-10-25 16:51:06 +0200

    vzalenost-mezi-prsty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+import math
 import keyboard as keyboard
 import mediapipe as mp
+
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
@@ -31,9 +33,22 @@ while True:
                         upCount += 1
                 if handList[thumb_Coord[0]][0] > handList[thumb_Coord[1]][0]:
                     upCount += 1
-                print(upCount)
+              #  print(upCount)
 
-            mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
+                distance = math.sqrt(pow(handList[8][0]- handList[4][0],2) + pow(handList[8][1] - handList[4][1],2))
+
+                print("--------")
+                print(handList[7])
+                print(handList[3])
+                print("---------")
+                print(abs(distance))
+
+
+                cv2.circle(image,handList[8], 25, (255, 0, 255), cv2.FILLED)
+                cv2.circle(image,handList[4], 25, (255, 0, 255), cv2.FILLED)
+
+
+                mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
     cv2.imshow("Output", image)
     cv2.waitKey(1)
     if keyboard.is_pressed("q") or keyboard.is_pressed("esc"):
