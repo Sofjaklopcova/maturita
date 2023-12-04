5935a618810a47c526b0a3879aab11f6f3e3e611
Sofjaklopcova <sofjk@seznam.cz>
2023-12-04 19:48:07 +0100

    zkouska_ramene

---
+++ arduino.py
@@ -0,0 +1,27 @@
+import serial
+import time
+com = "com6" # nastaveni portu arduina
+
+
+ArduinoSerial = serial.Serial(com, 9600)
+time.sleep(2)
+# ArduinoSerial.readline() - cteni z arduina
+# ArduinoSerial.write('1') - poslani do arduina
+# posilani hodnot do arduina, nasledne cteni z arduina (platformIO) a pak v platformIO to hybani
+# nebo to cele udelat v pythonu netusim jak protoze pristup k pinum jsem nenasla
+print("uvodni pozdrav")
+
+while 1:
+    user_input = input()
+    if (user_input == '1'):
+        time.sleep(1)
+        ArduinoSerial.write(bytes('1', 'UTF-8'))
+
+
+
+    if(user_input == '0'):
+        ArduinoSerial.write(bytes('0', 'UTF-8'))
+        time.sleep(1)
+
+
+# ArduinoSerial.close()
