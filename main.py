34df1a8c4d276c0c37e75ce5a0842878923e3029
Sofjaklopcova <sofjk@seznam.cz>
2023-12-15 14:43:37 +0100

    uprava-komunikavce

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import math
 import keyboard as keyboard
 import mediapipe as mp
 import serial
-import time
-count = 0
 
+count = 0
 upCount = 0
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
@@ -14,45 +13,41 @@ mpDraw = mp.solutions.drawing_utils
 finger_Coord = [(8, 6), (12, 10), (16, 14), (20, 18)]
 thumb_Coord = (4, 2)
 
-com = "com7"  # nastaveni portu arduina
+com = "com3"  # nastaveni portu arduina
 change = False
-delay = 5
-mode = 1
 arduinoSerial = serial.Serial(com, 9600)
 
 
-# distance - min: 2 max: 320 (me)
 
 
-def arduinoWrite():
-    global change, mode, arduinoSerial
+
+def arduinoWrite(mode):
+    global change, arduinoSerial
     if change is False:
         arduinoSerial.write(bytes(str(upCount), 'UTF-8'))
         print(upCount)
         if upCount == 1 or upCount == 2:
-            mode = upCount
-           # print("zmena modu")
-          #  change = True
+            change = True
+            print("cas mereni vzdalenosti ")
 
     else:
         arduinoSerial.write(bytes(str(distance), 'UTF-8'))
-        #print(distance)
+        print(distance)
         if (distance < 30 and mode == 1) or (distance > 50 and mode == 2):
-            print("zmena modu")
-          #  change = False
+            print("opousteni rezimu vzdalenosti")
+            change = False
     return
 
-def lowbudgettimer():
-    global count
-    for i in range(1000000000):
-        if(i == 99999999):
-            count += 1
-            lowbudgettimer()
-        if(count == 3):
-            arduinoWrite()
-            count = 0
+def write():
+    global count,upCount
+    if(upCount > 0):
+        count += 1
+    if(count == 150):
+        arduinoWrite(upCount)
+        count = 0
 
-lowbudgettimer()
+
+
 
 while True:
     success, image = cap.read()
@@ -80,12 +75,12 @@ while True:
                 if upCount > 1:
                     distance = math.sqrt(pow(handList[8][0] - handList[4][0], 2) + pow(handList[8][1] - handList[4][1], 2))
 
-                print("--------")
+                #print("--------")
                 #print(handList[7])
                 #print(handList[3])
                 #print("---------")
                 # print(abs(distance))
-
+                write()
                 cv2.circle(image, handList[8], 25, (255, 0, 255), cv2.FILLED)
                 cv2.circle(image, handList[4], 25, (255, 0, 255), cv2.FILLED)
 
